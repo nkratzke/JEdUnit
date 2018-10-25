8af5eb469aa475a6bcb8e5cd2db96e3917087d2f
Nane Kratzke <nkratzke@users.noreply.github.com>
2018-10-25 14:55:05 +0200

    Javaparser and jar handling preparation

--- vpl_evaluate.sh
+++ vpl_evaluate.sh
@@ -1,7 +1,10 @@
 cat > vpl_execution <<EEOOFF
 #! /bin/bash
-javac *.java
-java Checks
+
+wget --quiet http://central.maven.org/maven2/com/github/javaparser/javaparser-core/3.6.27/javaparser-core-3.6.27.jar
+
+javac -cp ".:*" Checks
+java  -cp ".:*" Checks
 EEOOFF
 
 chmod +x vpl_execution
