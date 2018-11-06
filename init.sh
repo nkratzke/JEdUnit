69eeb286d7a7cd831852127d2b59f4635f172316
Nane Kratzke <nkratzke@users.noreply.github.com>
2018-11-06 12:09:37 +0100

    Quiet init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-wget https://github.com/nkratzke/VPL-java-template/raw/working/build/libs/JEdUnit-0.1.10.jar -O JEdUnit.jar
+wget --quiet https://github.com/nkratzke/VPL-java-template/raw/working/build/libs/JEdUnit-0.1.10.jar
 java  -jar JEdUnit.jar
