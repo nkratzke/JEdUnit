2093616bc354c696e383b6016fb24f550d52f319
Nane Kratzke <nkratzke@users.noreply.github.com>
2018-11-06 12:08:01 +0100

    Version 0.1.10

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-wget https://github.com/nkratzke/VPL-java-template/raw/working/build/libs/JEdUnit-0.1.9.jar -O JEdUnit.jar
+wget https://github.com/nkratzke/VPL-java-template/raw/working/build/libs/JEdUnit-0.1.10.jar -O JEdUnit.jar
 java  -jar JEdUnit.jar
