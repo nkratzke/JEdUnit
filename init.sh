0ef49b223a31e9d394f493478dcff9dd5f8a6f88
Nane Kratzke <nkratzke@users.noreply.github.com>
2018-11-08 16:23:16 +0100

    init.sh Version 0.1.2

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-wget --quiet https://github.com/nkratzke/VPL-java-template/raw/working/build/libs/JEdUnit-0.1.10.jar
-java  -jar JEdUnit-0.1.10.jar
+wget --quiet https://github.com/nkratzke/VPL-java-template/raw/working/build/libs/JEdUnit-0.1.12.jar
+java  -jar JEdUnit-0.1.12.jar
