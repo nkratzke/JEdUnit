99f3a830a3f66a513c28dc0134657a0716c9cf2b
Nane Kratzke <nkratzke@users.noreply.github.com>
2018-11-08 16:38:09 +0100

    Latest version (0.1.13)

--- src/main/java/de/thl/jedunit/Evaluator.java
+++ src/main/java/de/thl/jedunit/Evaluator.java
@@ -31,7 +31,7 @@ public class Evaluator {
     /**
      * Version (Semantic Versioning).
      */
-    public static final String VERSION = "0.1.12";
+    public static final String VERSION = "0.1.13";
 
     /**
      * The maximum points for a VPL assignment.
