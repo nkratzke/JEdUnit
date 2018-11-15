23432d452fdaa602dfb040d40a392f0a3ff35dc9
Nane Kratzke <nkratzke@users.noreply.github.com>
2018-11-15 15:30:44 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-wget --quiet https://github.com/nkratzke/VPL-java-template/raw/working/build/libs/JEdUnit.jar
+wget --quiet https://github.com/nkratzke/VPL-java-template/raw/working/build/libs/JEdUnit.jar -O jedunit.jar
+wget --quiet https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.14/checkstyle-8.14-all.jar -O checkstyle.jar
+base64 -i jedunit.jar -o jedunit.jar.b64
+base64 -i checkstyle.jar -o checkstyle.jar.b64
 java  -jar JEdUnit.jar
