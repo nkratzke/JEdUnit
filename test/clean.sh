34cf6b5afbd85fba8f3e822e0ddf58fd356c6ae0
Nane Kratzke <nkratzke@users.noreply.github.com>
2018-11-20 10:25:52 +0100

    Update

--- test/clean.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash 
-rm -f *.class
-rm -f *.log
-rm -f vpl_execution
--- test/vpl_run.sh
+++
@@ -1,8 +0,0 @@
-cat > vpl_execution <<EEOOFF
-#! /bin/bash 
-rm -f *.class
-javac -cp ".:*" Main.java
-java -cp ".:*"  Main
-EEOOFF
-
-chmod +x vpl_execution
