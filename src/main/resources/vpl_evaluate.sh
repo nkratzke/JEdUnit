7d5a1c1b412a23aba492db64dad9f6bd350dc49d
Nane Kratzke <nkratzke@users.noreply.github.com>
2018-11-21 14:18:09 +0100

    Improved vpl_evaluate.sh (first compile - then checkstyle)

--- src/main/resources/vpl_evaluate.sh
+++ src/main/resources/vpl_evaluate.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 . common_script.sh
 rm -f *.class
-java  -jar checkstyle.jar -c style_checks.xml *.java > checkstyle.log
 javac -encoding UTF-8 -proc:none -cp ".:*" Checks.java
+java  -jar checkstyle.jar -c style_checks.xml *.java > checkstyle.log
 
 cat > vpl_execution <<EEOOFF
 #! /bin/bash
