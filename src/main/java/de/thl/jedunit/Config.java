668dd2aacce0c76cab0395ef00614d5f8cacacaa
Nane Kratzke <nkratzke@users.noreply.github.com>
2018-11-23 17:39:14 +0100

    Version 0.1.25

--- src/main/java/de/thl/jedunit/Config.java
+++ src/main/java/de/thl/jedunit/Config.java
@@ -15,7 +15,7 @@ public class Config {
     /**
      * Version of JEdUnit (Semantic Versioning).
      */
-    public final static String VERSION = "0.1.24"; 
+    public final static String VERSION = "0.1.25"; 
 
     /**
      * Set of file names that shall be considered by checkstyle and evaluation.
