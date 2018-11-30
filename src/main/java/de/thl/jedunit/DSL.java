7e03d0f405fb3621a7f2e2ce734838661d6d77d6
Nane Kratzke <nkratzke@users.noreply.github.com>
2018-11-30 16:07:29 +0100

    Update block commenting

--- src/main/java/de/thl/jedunit/DSL.java
+++ src/main/java/de/thl/jedunit/DSL.java
@@ -156,11 +156,11 @@ public class DSL {
      * Adds a comment for VPL via console output.
      */
     public static void comment(String c) {
-        if (c.contains("\n")) {
-            System.out.println("<|--");
-            System.out.println(c);
-            System.out.println("--|>");
-        } else System.out.println("Comment :=>>" + c);
+        // if (c.contains("\n")) {
+        System.out.println("<|--");
+        System.out.println(c);
+        System.out.println("--|>");
+        // } else System.out.println("Comment :=>>" + c);
     }
 
     /**
