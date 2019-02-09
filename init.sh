4c4d75309e3278d2f4ee341f8800de5b844f3286
Nane Kratzke <nkratzke@users.noreply.github.com>
2019-02-09 16:39:49 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "Downloading JEdUnit"
-wget --no-cache --quiet https://github.com/nkratzke/VPL-java-template/raw/working/build/libs/JEdUnit.jar -O jedunit.jar
+wget --no-cache --quiet https://github.com/nkratzke/JEdUnit/raw/working/build/libs/JEdUnit.jar -O jedunit.jar
 echo "Downloading Checkstyle"
 wget --no-cache --quiet https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.14/checkstyle-8.14-all.jar -O checkstyle.jar
 echo "Initializing assignment"
